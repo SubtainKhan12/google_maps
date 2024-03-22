5807632df54982f84628a07267ff1cd07eabcb61
SubtainKhan12 <subtainkhan5656@gmail.com>
2024-03-22 15:10:58 +0500

    window info

--- lib/Google Maps/windowInfo.dart
+++ lib/Google Maps/windowInfo.dart
@@ -1,9 +1,10 @@
-import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:custom_info_window/custom_info_window.dart';
 
 class WindowInfo extends StatefulWidget {
-  const WindowInfo({super.key});
+  const WindowInfo({Key? key}) : super(key: key);
 
   @override
   State<WindowInfo> createState() => _WindowInfoState();
@@ -12,129 +13,150 @@ class WindowInfo extends StatefulWidget {
 class _WindowInfoState extends State<WindowInfo> {
   CustomInfoWindowController _customInfoWindowController =
       CustomInfoWindowController();
-  final List<Marker> _markers = <Marker>[];
-  final List<LatLng> _latLang = [
-    LatLng(33.6941, 72.9734),
-    LatLng(33.7008, 72.9682),
-    LatLng(33.6992, 72.9744),
-    LatLng(33.6939, 72.9771),
-    LatLng(33.6910, 72.9807),
-    LatLng(33.7036, 72.9785)
-  ];
-  loadData() {
-    for (int i = 0; i < _latLang.length; i++) {
-      _markers.add(Marker(
-          markerId: MarkerId(i.toString()),
-          icon: BitmapDescriptor.defaultMarker,
-          position: _latLang[i],
-          onTap: () {
-            _customInfoWindowController.addInfoWindow!(
-                Container(
-                  width: 300,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 300,
-                        height: 100,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  'https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?cs=srgb&dl=pexels-narda-yescas-1566837.jpg&fm=jpg'),
-                              fit: BoxFit.fitWidth,
-                              filterQuality: FilterQuality.high),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
-                          color: Colors.red,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 100,
-                              child: Text(
-                                'Beef Tacos',
-                                maxLines: 1,
-                                overflow: TextOverflow.fade,
-                                softWrap: false,
-                                style: TextStyle(color: Colors.black),
-                              ),
-                            ),
-                            Spacer(),
-                            Text(
-                              '.3 mi.',
-                              style: TextStyle(color: Colors.black),
-                            )
-                          ],
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                        child: Text(
-                          'Help me finish these tacos! I got a platter from Costco and it’s too much.',
-                          maxLines: 2,
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                _latLang[i]);
-          }));
-      setState(() {});
-    }
-  }
+  late LatLng _userLocation = LatLng(33.6941, 72.9734); // Default location
+  late GoogleMapController _mapController;
+  bool _isMapReady = false;
+
+  List<Marker> _marker = <Marker>[];
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    loadData();
+    getUserLocation();
+  }
+
+  Future<void> getUserLocation() async {
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _userLocation = LatLng(position.latitude, position.longitude);
+      _marker.add(
+        Marker(
+          markerId: MarkerId("user_location"),
+          position: _userLocation,
+          onTap: () {
+            showInfoWindow();
+          },
+        ),
+      );
+    });
+
+    if (_isMapReady) {
+      _mapController.animateCamera(CameraUpdate.newLatLng(_userLocation));
+    }
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+    _customInfoWindowController.googleMapController = controller;
+    setState(() {
+      _isMapReady = true;
+    });
+  }
+
+  void showInfoWindow() {
+    _customInfoWindowController.addInfoWindow!(
+      Container(
+        width: 300,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 300,
+              height: 100,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://officesnapshots.com/wp-content/uploads/2018/09/software-house-offices-gliwice-12-1200x800.jpg",
+                  ),
+                  fit: BoxFit.fitWidth,
+                  filterQuality: FilterQuality.high,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+                color: Colors.red,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Text(
+                      'Crystal Solutions',
+                      maxLines: 1,
+                      overflow: TextOverflow.fade,
+                      softWrap: false,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '.3 mi.',
+                    style: TextStyle(color: Colors.black),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: Text(
+                'Challenge us with your requirnments',
+                maxLines: 2,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+      _userLocation,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          GoogleMap(
-              initialCameraPosition:
-                  CameraPosition(target: LatLng(33.6941, 72.9734), zoom: 14),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: _userLocation,
+                zoom: 14,
+              ),
               mapType: MapType.normal,
               zoomControlsEnabled: true,
               zoomGesturesEnabled: true,
               myLocationButtonEnabled: true,
-              myLocationEnabled: true,
+              // myLocationEnabled: true,
               trafficEnabled: false,
               rotateGesturesEnabled: true,
               buildingsEnabled: true,
-              markers: Set.of(_markers),
+              markers: Set.of(_marker),
               onTap: (position) {
                 _customInfoWindowController.hideInfoWindow!();
               },
               onCameraMove: (position) {
                 _customInfoWindowController.onCameraMove!();
               },
-              onMapCreated: (GoogleMapController controller) {
-                _customInfoWindowController.googleMapController = controller;
-              }),
-          CustomInfoWindow(
-            controller: _customInfoWindowController,
-            height: 200,
-            width: 300,
-            offset: 35,
-          )
-        ],
+              onMapCreated: onMapCreated,
+            ),
+            CustomInfoWindow(
+              controller: _customInfoWindowController,
+              height: 200,
+              width: 300,
+              offset: 35,
+            ),
+          ],
+        ),
       ),
     );
   }
