030729ce988e3913b68d887c826f71a801546aa5
SubtainKhan12 <subtainkhan5656@gmail.com>
2024-03-25 13:23:58 +0500

    picture in window info

--- lib/Google Maps/windowInfo.dart
+++ lib/Google Maps/windowInfo.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:custom_info_window/custom_info_window.dart';
+import 'package:image_picker/image_picker.dart';
 
 class WindowInfo extends StatefulWidget {
   const WindowInfo({Key? key}) : super(key: key);
@@ -17,6 +20,7 @@ class _WindowInfoState extends State<WindowInfo> {
   late GoogleMapController _mapController;
   bool _isMapReady = false;
   bool _locationPermissionDenied = false;
+  File? _imageFile;
 
   List<Marker> _marker = <Marker>[];
 
@@ -88,62 +92,45 @@ class _WindowInfoState extends State<WindowInfo> {
   void showInfoWindow() {
     _customInfoWindowController.addInfoWindow!(
       Container(
-        width: 300,
         height: 200,
+        width: 300,
         decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
+            borderRadius: BorderRadius.circular(12), color: Colors.white),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              width: 300,
-              height: 100,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    "https://officesnapshots.com/wp-content/uploads/2018/09/software-house-offices-gliwice-12-1200x800.jpg",
-                  ),
-                  fit: BoxFit.fitWidth,
-                  filterQuality: FilterQuality.high,
+            if (_imageFile != null)
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.file(
+                  _imageFile!,
+                  width: 300,
+                  height: 150,
+                  fit: BoxFit.cover,
                 ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-                color: Colors.red,
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    child: Text(
-                      'Crystal Solutions',
-                      maxLines: 1,
-                      // overflow: TextOverflow.fade,
-                      softWrap: false,
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    '.3 mi.',
-                    style: TextStyle(color: Colors.black),
-                  )
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+            Padding(
+              padding: const EdgeInsets.only(top: 5, left: 10),
               child: Text(
-                'Challenge us with your requirements',
-                maxLines: 2,
-                style: TextStyle(color: Colors.black),
+                "Upload Picture",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            GestureDetector(
+              onTap: _takePicture,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 5, left: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 164, 147, 192),
+                  ),
+                  child: Icon(
+                    Icons.camera,
+                    size: 35,
+                  ),
+                ),
               ),
             ),
           ],
@@ -153,49 +140,53 @@ class _WindowInfoState extends State<WindowInfo> {
     );
   }
 
+  void _takePicture() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      showInfoWindow();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: _locationPermissionDenied
-            ? Center(
-                child: Text(
-                  'Location permission denied.',
-                  style: TextStyle(fontSize: 18),
-                ),
-              )
-            : Stack(
-                children: [
-                  GoogleMap(
-                    initialCameraPosition: CameraPosition(
-                      target: _userLocation,
-                      zoom: 14,
-                    ),
-                    mapType: MapType.normal,
-                    zoomControlsEnabled: true,
-                    zoomGesturesEnabled: true,
-                    myLocationButtonEnabled: true,
-                    // myLocationEnabled: true,
-                    trafficEnabled: false,
-                    rotateGesturesEnabled: true,
-                    buildingsEnabled: true,
-                    markers: Set.of(_marker),
-                    onTap: (position) {
-                      _customInfoWindowController.hideInfoWindow!();
-                    },
-                    onCameraMove: (position) {
-                      _customInfoWindowController.onCameraMove!();
-                    },
-                    onMapCreated: onMapCreated,
-                  ),
-                  CustomInfoWindow(
-                    controller: _customInfoWindowController,
-                    height: 200,
-                    width: 300,
-                    offset: 35,
-                  ),
-                ],
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: _userLocation,
+                zoom: 14,
               ),
+              mapType: MapType.normal,
+              zoomControlsEnabled: true,
+              zoomGesturesEnabled: true,
+              myLocationButtonEnabled: true,
+              trafficEnabled: false,
+              rotateGesturesEnabled: true,
+              buildingsEnabled: true,
+              markers: Set.of(_marker),
+              onTap: (position) {
+                _customInfoWindowController.hideInfoWindow!();
+              },
+              onCameraMove: (position) {
+                _customInfoWindowController.onCameraMove!();
+              },
+              onMapCreated: onMapCreated,
+            ),
+            CustomInfoWindow(
+              controller: _customInfoWindowController,
+              height: 250,
+              width: 300,
+              offset: 35,
+            ),
+          ],
+        ),
       ),
     );
   }
