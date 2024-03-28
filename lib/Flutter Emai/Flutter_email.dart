927e595060f2a705e1f8b59cca2e272b3c2d94bf
SubtainKhan12 <subtainkhan5656@gmail.com>
2024-03-28 14:27:29 +0500

    emailsending through api

--- lib/Flutter Emai/Flutter_email.dart
+++ lib/Flutter Emai/Flutter_email.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class EmailSending extends StatefulWidget {
   const EmailSending({super.key});
@@ -9,7 +11,6 @@ class EmailSending extends StatefulWidget {
 }
 
 class _EmailSendingState extends State<EmailSending> {
-  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _subjectController = TextEditingController();
   final TextEditingController _bodyController = TextEditingController();
   final TextEditingController _sendingtoController = TextEditingController();
@@ -52,7 +53,7 @@ class _EmailSendingState extends State<EmailSending> {
           Padding(
             padding: const EdgeInsets.only(right: 10.0),
             child: IconButton(
-              onPressed: () {},
+              onPressed: _sendEmail,
               icon: Icon(
                 Icons.send,
                 color: Colors.white,
@@ -66,23 +67,6 @@ class _EmailSendingState extends State<EmailSending> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  hintText: "Email...",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.black),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
               TextField(
                 controller: _sendingtoController,
                 decoration: InputDecoration(
@@ -163,5 +147,37 @@ class _EmailSendingState extends State<EmailSending> {
         ),
       ),
     );
+  }
+
+  Future<void> _sendEmail() async {
+    if (_filePath == null) {
+      print("No file selected for attachment.");
+      return;
+    }
+
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('https://crystalsolutions.com.pk/test/EmailSend.php'),
+    );
+    request.fields['email'] = _sendingtoController.text;
+    request.fields['subject'] = _subjectController.text;
+    request.fields['body'] = _bodyController.text;
+
+    var file = await http.MultipartFile.fromPath('file', _filePath!);
+    request.files.add(file);
+
+    try {
+      var streamedResponse = await request.send();
+      var response = await http.Response.fromStream(streamedResponse);
+      var result = jsonDecode(response.body);
+
+      if (result["error"] == 200) {
+        print(result["message"]);
+      } else {
+        print(result["error"]);
+      }
+    } catch (e) {
+      print("Error sending email: $e");
+    }
   }
 }
